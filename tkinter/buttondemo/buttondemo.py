d3a35704cc75207c4d5cb197135d0583e2748fe8
HDRorz <ab250985201@gmail.com>
2015-07-15 15:33:41 +0800

    a new project
    a project of tk include a button to run a exe

---
+++ tkinter/buttondemo/buttondemo.py
@@ -0,0 +1,33 @@
+#coding=utf-8 
+import Tkinter
+import os
+from subprocess import Popen
+# import sys
+
+# reload(sys)
+# sys.setdefaultencoding( "utf-8" )
+
+class TkButtonDemo:
+	def __init__(self):
+		self.top = Tkinter.Tk()
+		self.top.title(u'按钮demo')
+		self.top.geometry('500x500')
+		#self.iconbitmap('root')
+		
+		self.button1 = Tkinter.Button(self.top, text=u'任务1', command=self.Button1Click)
+		self.button1.pack()
+		
+	def Button1Click(self):
+		b1cmd = r'cmd /min E:\大二\大二第一学期\C++\0.0.exe'
+		#result = os.system(b1cmd)
+		#result = os.system(u'start E:\大二\大二第一学期\C++\0.0.exe')
+		process = Popen(b1cmd)
+
+
+
+def main():
+	obj = TkButtonDemo()
+	Tkinter.mainloop()		
+	
+if __name__ == '__main__':
+	main()
